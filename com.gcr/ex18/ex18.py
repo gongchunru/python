964430e895889125a13e8cf879bdde53cf1964dd
gongchunru <gongchunru@163.com>
2017-05-05 00:52:24 +0800

    add ex20

--- com.gcr/ex18/ex18.py
+++ com.gcr/ex18/ex18.py
@@ -11,13 +11,13 @@ def print_two_again(arg1,arg2):
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
 
-def print_one(arg1):
+def _print_one(arg1) :
     print "arg1: %r" % arg1
 
 def print_none():
     print "I got nothin!"
 
-print_one("One")
+_print_one ("One")
 print_none()
 print_two("zed","Shaw")
 print_two_again("zed","Ahaw")
